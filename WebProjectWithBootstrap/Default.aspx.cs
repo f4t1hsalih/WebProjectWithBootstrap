eb2433dcfb1a1677d20366aa0276e73e96882aee
f4t1hsalih <f4t1hsalih@gmail.com>
2024-04-20 17:56:25 +0300

    Lecture 21 - Öğrenci Verileri Çekilmesi/2

--- WebProjectWithBootstrap/Default.aspx.cs
+++ WebProjectWithBootstrap/Default.aspx.cs
@@ -11,9 +11,9 @@ namespace WebProjectWithBootstrap
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            DataSetTableAdapters.tbl_studentsTableAdapter dt = new DataSetTableAdapters.tbl_studentsTableAdapter();
-            Repeater1.DataSource = dt.StudentList();
-            Repeater1.DataBind();
+            //DataSetTableAdapters.tbl_studentsTableAdapter dt = new DataSetTableAdapters.tbl_studentsTableAdapter();
+            //Repeater1.DataSource = dt.StudentList();
+            //Repeater1.DataBind();
         }
     }
 }
